93d77f18fdf69ce221574e8381324feaff6ae1a5
adibcoding <muhammadadibhasany1501@gmail.com>
2023-07-01 16:33:53 +0700

    update: add stripe payment

--- FP-Mobile-Client/App.js
+++ FP-Mobile-Client/App.js
@@ -4,26 +4,32 @@ import Run from "./src/screens/run";
 import MainStack from "./src/navigators/stackNav";
 import DashboardTabs from "./src/navigators/dashboardTabs.js";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import PaymentScreen from "./src/screens/payment";
+import { publishableKey } from "./src/config/apiKey";
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#1E87CE",
-          },
-          headerTitleAlign: "center",
-          headerTitleStyle: {
-            color: "#fff",
-            fontWeight: "bold",
-          },
-        }}
-      >
-        <Stack.Screen name="Login" component={MainStack} />
-        <Stack.Screen name="Dashboard" component={DashboardTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StripeProvider publishableKey={publishableKey}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#1E87CE",
+            },
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              color: "#fff",
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen name="Login" component={MainStack} />
+          <Stack.Screen name="Dashboard" component={DashboardTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StripeProvider>
   );
 }
 
